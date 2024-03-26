23b32873574695e34c1321aaded092987e59d219
democrazyfi <democrazyfi@gmail.com>
2024-03-26 17:45:23 +0400

    fix css file typo

--- frontend/components/instructionsComponent/navigation/navbar/index.tsx
+++ frontend/components/instructionsComponent/navigation/navbar/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import WalletConnector from "@/components/wallet/WalletConnector";
-import styles from "./Navbar.module.css";
+import styles from "./navbar.module.css";
 import { Spacer } from "@chakra-ui/react";
 export default function Navbar() {
   return (
