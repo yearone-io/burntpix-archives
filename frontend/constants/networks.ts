a295c4e2c130a268f7f00515ad215797de7f42e6
Ledespana <no-reply@ledespana.com>
2024-03-29 12:47:08 +0100

    linksg

--- frontend/constants/networks.ts
+++ frontend/constants/networks.ts
@@ -13,7 +13,7 @@ const NETWORKS = {
     symbol: "LYX",
     rpcUrl: "https://lukso.rpc.thirdweb.com",
     explorerURL: "https://explorer.execution.mainnet.lukso.network",
-    baseUrl: " ", // TODO add mainnet base url
+    baseUrl: "https://burntpix-archives.netlify.app",
   },
   testnet: {
     chainId: 4201,
@@ -21,7 +21,7 @@ const NETWORKS = {
     symbol: "LYXt",
     rpcUrl: "https://lukso-testnet.rpc.thirdweb.com",
     explorerURL: "https://explorer.execution.testnet.lukso.network",
-    baseUrl: " ", // TODO add testnet base url
+    baseUrl: "https://testnet--burntpix-archives.netlify.app", 
   },
 } as {
   [key: string]: Network;
