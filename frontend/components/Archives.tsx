8c452a977c2737b5426101f24717cd46de30768b
democrazyfi <democrazyfi@gmail.com>
2024-04-02 17:23:05 +0400

    support not minted

--- frontend/components/Archives.tsx
+++ frontend/components/Archives.tsx
@@ -23,9 +23,9 @@ import { constants } from "@/constants/constants";
 interface IArchive {
   id: string;
   image: string;
-  ownerName: string;
-  ownerAddress: string;
-  ownerAvatar: string | undefined;
+  ownerName?: string;
+  ownerAddress?: string;
+  ownerAvatar?: string;
   isMinted: boolean;
 }
 
@@ -62,20 +62,38 @@ const Archives = ({ images }: { images: string[] }) => {
           Array.from({ length: Number(archiveCount) }, async (_, i) => {
             const id = numberToBytes32(i + 1);
             const archive = await burntPixArchives.burntArchives(id);
-            const owner = await burntPixArchives.tokenOwnerOf(id);
-            const ownerProfile = await getProfileData(
-              owner,
-              networkConfig.rpcUrl,
-            );
+            let isMinted = false;
+            let ownerName = undefined;
+            let ownerAddress = undefined;
+            let ownerAvatar = undefined;
+            await burntPixArchives
+              .tokenOwnerOf(id)
+              .then(async (ownerAddress) => {
+                isMinted = true;
+                return await getProfileData(ownerAddress, networkConfig.rpcUrl);
+              })
+              .then((ownerProfile) => {
+                if (ownerProfile.profileImage) {
+                  ownerAvatar = `${constants.IPFS_GATEWAY}/${ownerProfile.profileImage[0].url.replace("ipfs://", "")}`;
+                }
+                ownerName = ownerProfile.name;
+              })
+              .catch((reason) => {
+                if (reason.message.includes("LSP8NonExistentTokenId")) {
+                  console.log(`token ${id} not minted yet`);
+                } else {
+                  console.error("Error fetching owner", reason);
+                  throw reason;
+                }
+              });
+
             fetchedArchives.push({
               id: id,
               image: hexToText(archive.image),
-              ownerAddress: owner,
-              ownerName: ownerProfile.name,
-              ownerAvatar: ownerProfile.profileImage
-                ? `${constants.IPFS_GATEWAY}/${ownerProfile.profileImage[0].url.replace("ipfs://", "")}`
-                : undefined,
-              isMinted: false,
+              ownerAddress: ownerAddress,
+              ownerName: ownerName,
+              ownerAvatar: ownerAvatar,
+              isMinted: isMinted,
             });
             return archive;
           }),
