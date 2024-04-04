d6511fc7df26ff10b8b1cec93ab03f7847463f59
Ledespana <no-reply@ledespana.com>
2024-04-04 11:20:33 +0200

    initial

--- frontend/components/RefineButton.tsx
+++ frontend/components/RefineButton.tsx
@@ -1,34 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Button, Flex, Icon } from "@chakra-ui/react";
 import { MdSettings } from "react-icons/md";
 import { inter } from "@/app/fonts";
+import { WalletContext } from "@/components/wallet/WalletContext";
 
 const RefineButton: React.FC = () => {
-  const buttonBgColor = "#FE005B";
   const buttonTextColor = "white";
   const iconButtonSize = "24px";
+  const walletContext = useContext(WalletContext);
+  const { account } = walletContext;
+  const buttonBgColor = account ? "#FE005B": 'grey';
+  const hoverBgColor = account ? "red.600" : 'grey';
+  const buttonText= account ? "REFINE" : "CONNECT WALLET";
 
   return (
     <Flex align="center" justify="right">
       <Button
         bg={buttonBgColor}
         color={buttonTextColor}
-        _hover={{ bg: "red.600" }}
+        _hover={{ bg: hoverBgColor }}
         borderRadius={10}
         h="30px"
-        w="70px"
+        w='fit-content'
         fontSize="md"
         fontFamily={inter.style.fontFamily}
         fontWeight={700}
       >
-        REFINE
+        {buttonText}
       </Button>
 
       <Button
         size="sm"
         variant="ghost"
         color={buttonBgColor}
-        _hover={{ color: "red.600" }}
+        _hover={{ color: hoverBgColor }}
         p={0}
       >
         <Icon as={MdSettings} boxSize={iconButtonSize} />
