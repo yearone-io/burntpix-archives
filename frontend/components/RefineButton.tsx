83b24dfc877706a32ff8a5061ec970d76b8f1fc4
Ledespana <no-reply@ledespana.com>
2024-04-04 11:45:16 +0200

    d

--- frontend/components/RefineButton.tsx
+++ frontend/components/RefineButton.tsx
@@ -1,44 +1,65 @@
 import React, { useContext } from "react";
-import { Button, Flex, Icon } from "@chakra-ui/react";
+import { Box, Button, Flex, Icon, useToast } from "@chakra-ui/react";
 import { MdSettings } from "react-icons/md";
 import { inter } from "@/app/fonts";
 import { WalletContext } from "@/components/wallet/WalletContext";
+import { BurntPixArchives__factory } from "@/contracts";
+import SignInButton from "./SignInButton";
 
 const RefineButton: React.FC = () => {
+  const walletContext = useContext(WalletContext);
+  const { account, provider, isLoadingAccount, networkConfig } = walletContext;
   const buttonTextColor = "white";
   const iconButtonSize = "24px";
-  const walletContext = useContext(WalletContext);
-  const { account } = walletContext;
-  const buttonBgColor = account ? "#FE005B": 'grey';
-  const hoverBgColor = account ? "red.600" : 'grey';
-  const buttonText= account ? "REFINE" : "CONNECT WALLET";
+  const buttonBgColor = account ? "#FE005B" : "grey";
+  const hoverBgColor = account ? "red.600" : "grey";
+  const buttonText = account ? "REFINE" : "CONNECT WALLET";
+
+  const toast = useToast();
+
+  const burntPixArchives = BurntPixArchives__factory.connect(
+    networkConfig.burntPixArchivesAddress,
+    provider,
+  );
+
+  const refine = async () => {};
 
   return (
-    <Flex align="center" justify="right">
-      <Button
-        bg={buttonBgColor}
-        color={buttonTextColor}
-        _hover={{ bg: hoverBgColor }}
-        borderRadius={10}
-        h="30px"
-        w='fit-content'
-        fontSize="md"
-        fontFamily={inter.style.fontFamily}
-        fontWeight={700}
-      >
-        {buttonText}
-      </Button>
+    <Box>
+      {account ? (
+        <Flex align="center" justify="right">
+          <Button
+            bg={buttonBgColor}
+            color={buttonTextColor}
+            _hover={{ bg: hoverBgColor }}
+            borderRadius={10}
+            h="30px"
+            w="fit-content"
+            fontSize="md"
+            fontFamily={inter.style.fontFamily}
+            fontWeight={700}
+            onClick={refine}
+          >
+            {buttonText}
+          </Button>
 
-      <Button
-        size="sm"
-        variant="ghost"
-        color={buttonBgColor}
-        _hover={{ color: hoverBgColor }}
-        p={0}
-      >
-        <Icon as={MdSettings} boxSize={iconButtonSize} />
-      </Button>
-    </Flex>
+          <Button
+            size="sm"
+            variant="ghost"
+            color={buttonBgColor}
+            _hover={{ color: hoverBgColor }}
+            p={0}
+          >
+            <Icon as={MdSettings} boxSize={iconButtonSize} />
+          </Button>
+        </Flex>
+      ) : (
+        <Flex align="center" justify="right">
+
+        <SignInButton />
+        </Flex>
+      )}
+    </Box>
   );
 };
 
