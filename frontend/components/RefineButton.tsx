334ce459d44a2e91724c3bd8d4f37cc3b1a11610
Ledespana <12192218+Ledespana@users.noreply.github.com>
2024-04-05 13:57:22 +0200

    Refine (#38)
    * initial
    
    * d
    
    * pro pro pro progreess
    
    * refine
    
    * refactor
    
    * min 1
    
    * feedback
    
    * feedback
    
    * feedback
    
    ---------
    
    Co-authored-by: Ledespana <no-reply@ledespana.com>

--- frontend/components/RefineButton.tsx
+++ frontend/components/RefineButton.tsx
@@ -1,39 +1,166 @@
-import React from "react";
-import { Button, Flex, Icon } from "@chakra-ui/react";
+import React, { useContext, useState, useEffect } from "react";
+import {
+  Box,
+  Button,
+  Flex,
+  Icon,
+  useToast,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverBody,
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  Text,
+} from "@chakra-ui/react";
 import { MdSettings } from "react-icons/md";
+import { WalletContext } from "@/components/wallet/WalletContext";
+import { BurntPixArchives__factory } from "@/contracts";
+import SignInButton from "./SignInButton";
 import { inter } from "@/app/fonts";
 
 const RefineButton: React.FC = () => {
-  const buttonBgColor = "#FE005B";
-  const buttonTextColor = "white";
-  const iconButtonSize = "24px";
+  const walletContext = useContext(WalletContext);
+  const { account, provider, networkConfig } = walletContext;
+  const defaultIterations = 100;
+  const [selectedIterations, setSelectedIterations] =
+    useState(defaultIterations);
+  const [isRefining, setIsRefining] = useState(false);
+  const toast = useToast();
+  const maxIterations = 10000;
+  const defaultRed = "#FE005B";
+  const burntPixArchives = BurntPixArchives__factory.connect(
+    networkConfig.burntPixArchivesAddress,
+    provider,
+  );
+
+  const refine = async () => {
+    setIsRefining(true);
+    try {
+      console.log("Refining with iterations: ", selectedIterations);
+      const signer = await provider.getSigner();
+      await burntPixArchives
+        .connect(signer)
+        ["refineToArchive(uint256)"](selectedIterations);
+      setIsRefining(false);
+    } catch (error: any) {
+      setIsRefining(false);
+      let message = "Error refining: ";
+      if (error.code === "ACTION_REJECTED") {
+        message += "Transaction rejected.";
+      } else if (error.action === "estimateGas") {
+        message +=
+          "Error estimating gas, try with a lower number of iterations.";
+      } else {
+        message += error.message;
+      }
+      toast({
+        title: message,
+        status: "error",
+        position: "bottom-left",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+  };
 
   return (
-    <Flex align="center" justify="right">
-      <Button
-        bg={buttonBgColor}
-        color={buttonTextColor}
-        _hover={{ bg: "red.600" }}
-        borderRadius={10}
-        h="30px"
-        w="70px"
-        fontSize="md"
-        fontFamily={inter.style.fontFamily}
-        fontWeight={700}
-      >
-        REFINE
-      </Button>
-
-      <Button
-        size="sm"
-        variant="ghost"
-        color={buttonBgColor}
-        _hover={{ color: "red.600" }}
-        p={0}
-      >
-        <Icon as={MdSettings} boxSize={iconButtonSize} />
-      </Button>
-    </Flex>
+    <Box>
+      {account ? (
+        <Flex align="center" justify="right">
+          <Button
+            bg={defaultRed}
+            color="white"
+            _hover={{ bg: defaultRed }}
+            borderRadius={10}
+            h="30px"
+            w="fit-content"
+            fontSize="md"
+            fontWeight={700}
+            onClick={refine}
+            fontFamily={inter.style.fontFamily}
+            loadingText={"REFINING..."}
+            isLoading={isRefining}
+          >
+            REFINE TO ARCHIVE
+          </Button>
+          <Popover placement="top">
+            <PopoverTrigger>
+              <Button
+                size="sm"
+                variant="ghost"
+                p={0}
+                ml={2}
+                isLoading={isRefining}
+              >
+                <Icon as={MdSettings} boxSize="24px" color={defaultRed} />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent p="10px">
+              <PopoverArrow />
+              <PopoverCloseButton />
+              <PopoverBody>
+                <Text
+                  mb="4"
+                  fontWeight="bold"
+                  fontFamily={inter.style.fontFamily}
+                >
+                  Adjust Iterations:
+                </Text>
+                <Flex flexDir="column">
+                  <NumberInput
+                    mb="4"
+                    defaultValue={defaultIterations}
+                    min={1}
+                    max={maxIterations}
+                    maxW="100px"
+                    mr="2rem"
+                    value={selectedIterations}
+                    onChange={(value: string) => {
+                      setSelectedIterations(Number(value));
+                    }}
+                  >
+                    <NumberInputField />
+                    <NumberInputStepper>
+                      <NumberIncrementStepper />
+                      <NumberDecrementStepper />
+                    </NumberInputStepper>
+                  </NumberInput>
+                  <Slider
+                    flex="1"
+                    defaultValue={defaultIterations}
+                    min={1}
+                    max={maxIterations}
+                    step={1}
+                    focusThumbOnChange={false}
+                    value={selectedIterations}
+                    onChange={setSelectedIterations}
+                  >
+                    <SliderTrack bg="gray.200">
+                      <SliderFilledTrack bg={defaultRed} />
+                    </SliderTrack>
+                    <SliderThumb boxSize="15px" bg={defaultRed} />
+                  </Slider>
+                </Flex>
+              </PopoverBody>
+            </PopoverContent>
+          </Popover>
+        </Flex>
+      ) : (
+        <Flex align="center" justify="right">
+          <SignInButton />
+        </Flex>
+      )}
+    </Box>
   );
 };
 
