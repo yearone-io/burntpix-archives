4f66c7334f05ca06ddc58b824990835e524f1dcf
user.name <user@email>
2024-04-09 17:25:18 +0300

    clean up

--- frontend/components/Home.tsx
+++ frontend/components/Home.tsx
@@ -11,7 +11,7 @@ import { ContributionsRow } from "./ContributionsRow";
 
 export default function Home() {
   const walletContext = useContext(WalletContext);
-  const { account, networkConfig, provider, refineEventCounter } =
+  const { account, networkConfig, provider } =
     walletContext;
   const toast = useToast();
   const burntPixArchives = BurntPixArchives__factory.connect(
