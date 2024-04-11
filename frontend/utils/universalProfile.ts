0039434c1019f9e8f2d69c38b01b9467fcc90295
tehnalogos <128236767+tehnalogos@users.noreply.github.com>
2024-04-11 09:41:49 +0200

    Update universalProfile.ts

--- frontend/utils/universalProfile.ts
+++ frontend/utils/universalProfile.ts
@@ -14,7 +14,11 @@ export const buildSIWEMessage = (upAddress: string): string => {
       "Welcome Welcome my Friiiend, please make sure you have read and understood our terms of service and conditions and privacy policy. By signing in, you confirm that you have read and agree to these documents and will use the platform in accordance with their provisions. Thank you for using Universal GRAVE, and we hope we solve all your spam problems once and for all.", // a human-readable assertion user signs
     version: "1", // Current version of the SIWE Message
     chainId: getNetworkConfig(process.env.NEXT_PUBLIC_DEFAULT_NETWORK!).chainId, // Chain ID to which the session is bound, 4201 is LUKSO Testnet
-    resources: [], // Information the user wishes to have resolved as part of authentication by the relying party
+    resources: [
+      `${window.location.origin}/terms`,
+      `${window.location.origin}/terms#disclaimer`,
+      `${window.location.origin}/terms#privacy`,
+    ], // Information the user wishes to have resolved as part of authentication by the relying party
   };
   return new SiweMessage(siweParams).prepareMessage();
 };
