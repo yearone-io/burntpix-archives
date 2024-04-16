8619366389c8068df6cdd51f6a7063d92b384d98
democrazyfi <democrazyfi@gmail.com>
2024-04-16 16:58:07 +0400

    cleanup

--- frontend/components/SignInButton.tsx
+++ frontend/components/SignInButton.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Box, Button, Flex, Image, useToast } from "@chakra-ui/react";
+import { Box, Button, Flex, Image } from "@chakra-ui/react";
 import { WalletContext } from "./wallet/WalletContext";
 import { inter } from "@/app/fonts";
 
@@ -7,46 +7,11 @@ const SignInButton: React.FC = () => {
   const walletContext = useContext(WalletContext);
   const { connect, isLoadingAccount, networkConfig, connectedChainId } =
     walletContext;
-  const toast = useToast();
   const fontDimensions = { base: "0.75rem", md: "0.85rem" };
 
-  const onSignInClick = async () => {
-    if (connectedChainId !== networkConfig.chainId) {
-      try {
-        await window.lukso.request({
-          method: "wallet_switchEthereumChain",
-          params: [
-            { chainId: "0x" + BigInt(networkConfig.chainId).toString(16) },
-          ],
-        });
-      } catch (error: any) {
-        toast({
-          title: `Error switching network. ${error.message}`,
-          status: "error",
-          position: "bottom-left",
-          duration: 5000,
-          isClosable: true,
-        });
-        return;
-      }
-    }
-    try {
-      await connect();
-    } catch (error: any) {
-      toast({
-        title: `Failed to connect wallet: ${error.message}`,
-        status: "error",
-        position: "bottom-left",
-        duration: 5000,
-        isClosable: true,
-      });
-    }
-  };
-
-  const buttonText = connectedChainId !== networkConfig.chainId ? "Switch Network" : "Sign In";
   return (
     <Button
-      onClick={onSignInClick}
+      onClick={connect}
       border={"1px solid #000000"}
       backgroundColor={"transparent"}
       fontFamily={inter.style.fontFamily}
@@ -65,7 +30,7 @@ const SignInButton: React.FC = () => {
           height={fontDimensions}
         />
         <Box fontSize={fontDimensions} lineHeight={fontDimensions}>
-          {isLoadingAccount ? "..." : buttonText}
+          {isLoadingAccount ? "..." : "SIGN IN"}
         </Box>
       </Flex>
     </Button>
